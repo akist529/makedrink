9294ab7e41e016a6e36919f905de1e092338b7da
Alex Kist <akist0707@gmail.com>
2023-03-21 14:22:59 -0500

    Add button to nav menu to select ingredients

--- src/components/navmenu/NavMenu.tsx
+++ src/components/navmenu/NavMenu.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
 import styles from './NavMenu.module.scss'
+import NavMenuCategory from '@/components/navmenu/category/NavMenuCategory'
+import NavMenuItem from '@/components/navmenu/item/NavMenuItem'
 
 export default function NavMenu (props: any) {
     const spirits = ['Bourbon', 'Brandy', 'Gin', 'Rum', 'Scotch', 'Tequila', 'Vermouth', 'Vodka', 'Whiskey']
@@ -8,21 +9,8 @@ export default function NavMenu (props: any) {
     return (
         <nav className={[styles.navmenu, (navMenuOpen ?  styles.open : styles.closed)].join(' ')}>
             <ul>
-                <li>
-                    <span>Spirits</span>
-                    <ul>
-                        { spirits.map(spirit => {
-                            return (
-                                <li key={spirit}>
-                                    <button>
-                                        <span>{spirit}</span>
-                                        <Image alt={spirit} src={require(`/public/images/ui/${spirit.toLowerCase()}.webp`)} width='48' height='48' />
-                                    </button>
-                                </li>
-                            )
-                        }) }
-                    </ul>
-                </li>
+                <NavMenuItem item='Select Ingredients' />
+                <NavMenuCategory category='Spirits' items={spirits} />
             </ul>
         </nav>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ export default function HomePage() {
       <NavMenu navMenuOpen={navMenuOpen} />
       <div>Welcome to Next.js!</div>
       <footer>
+        <a>Bartender icon created by Iconduck</a>
         <a href="https://www.flaticon.com/free-icons" title="spirit icons">Spirit icons created by Freepik - Flaticon</a>
       </footer>
     </div>
