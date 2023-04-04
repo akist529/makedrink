64d2243f57879ff374bec93260f9755c4a578d75
Alex Kist <akist0707@gmail.com>
2023-04-04 10:33:31 -0500

    Crop all .webp files to image content

--- src/components/ui/IngredientSection/IngredientSection.tsx
+++ src/components/ui/IngredientSection/IngredientSection.tsx
@@ -1,12 +1,14 @@
 import styles from './IngredientSection.module.scss'
 import Image from 'next/image'
 import IngredientCheckbox from '../../../components/inputs/IngredientCheckbox/IngredientCheckbox'
+import { Item } from '@/types/index'
 
-export default function IngredientSection (props: any) {
+export default function IngredientSection (props: {section: Item[]}) {
     const {section} = props
 
-    const sortedSection = (function sortNames () {
-        const sorted = section.sort(function (a: any, b: any) {
+    // Sort ingredients alphabetically before rendering to DOM
+    const sortedSection = (() => {
+        const sorted = section.sort(function (a: Item, b: Item) {
             if (a['Name'] < b['Name']) {
                 return -1
             } else {
@@ -19,15 +21,17 @@ export default function IngredientSection (props: any) {
 
     return (
         <ul className={styles.IngredientSection}>
-            {sortedSection.map((item: any) => {
+            {sortedSection.map((item: Item) => {
                 return (
-                    <li key={item['Name']} className={styles.Ingredient}>
-                        <div className={styles.children}>
-                            <div></div>
-                        </div>
-                        <div className={styles.name}>
-                            <span>{item['Name']}</span>
-                            <Image alt={item['Name']} src={require(`/public/images/ui/${item['Name'].toLowerCase().split(" ").join("-").replaceAll("/", "-")}.webp`)} />
+                    <li key={item['Id']} className={styles.Ingredient}>
+                        <div className={styles.info}>
+                            <button>
+                                <Image alt="Show Varieties" src={require(`/public/images/ui/more_vert.svg`)} width="8" height="64" />
+                            </button>
+                            <div className={styles.name}>
+                                <span>{item['Name']}</span>
+                                <Image alt={item['Name']} src={require(`/public/images/ui/${item['Name'].toLowerCase().split(" ").join("-").replaceAll("/", "-")}.webp`)} />
+                            </div>
                         </div>
                         <IngredientCheckbox item={item} />
                     </li>
