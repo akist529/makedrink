7171aa13a6e8361ce83b3cf2cce345c459f2fd78
Alex Kist <akist0707@gmail.com>
2023-04-04 11:01:44 -0500

    Filter ingredients to show only ingredients that are not variants

--- src/components/ui/Ingredient/Ingredient.tsx
+++ src/components/ui/Ingredient/Ingredient.tsx
@@ -2,16 +2,26 @@ import { Item } from '@/types/index'
 import styles from './Ingredient.module.scss'
 import Image from 'next/image'
 import IngredientCheckbox from '@/components/inputs/IngredientCheckbox/IngredientCheckbox'
+import { useState, useEffect } from 'react'
 
-export default function Ingredient (props: { item: Item }) {
-    const { item } = props
+export default function Ingredient (props: { item: Item, section: Item[] }) {
+    const { item, section } = props
+    const [hasChildren, setHasChildren] = useState(false)
+
+    useEffect(() => {
+        section.forEach(ingredient => {
+            if (ingredient['AliasId'] === item['Id']) {
+                setHasChildren(true)
+            }
+        })
+    }, [])
 
     return (
         <li className={styles.Ingredient}>
             <div className={styles.info}>
-                <button>
+                { hasChildren && <button>
                     <Image alt="Show Varieties" src={require(`/public/images/ui/more_vert.svg`)} width="8" height="64" />
-                </button>
+                </button> }
                 <div className={styles.name}>
                     <span>{item['Name']}</span>
                     <Image alt={item['Name']} src={require(`/public/images/ui/${item['Name'].toLowerCase().split(" ").join("-").replaceAll("/", "-")}.webp`)} />
--- src/components/ui/IngredientSection/IngredientSection.tsx
+++ src/components/ui/IngredientSection/IngredientSection.tsx
@@ -6,9 +6,14 @@ import { Item } from '@/types/index'
 export default function IngredientSection (props: {section: Item[]}) {
     const {section} = props
 
+    // Remove ingredients that are variants of another ingredient
+    const filteredSection = (() => {
+        return section.filter(item => item['AliasId'] === 0)
+    })()
+
     // Sort ingredients alphabetically before rendering to DOM
     const sortedSection = (() => {
-        const sorted = section.sort(function (a: Item, b: Item) {
+        const sorted = filteredSection.sort(function (a: Item, b: Item) {
             if (a['Name'] < b['Name']) {
                 return -1
             } else {
@@ -23,7 +28,7 @@ export default function IngredientSection (props: {section: Item[]}) {
         <ul className={styles.IngredientSection}>
             {sortedSection.map((item: Item) => {
                 return (
-                    <Ingredient key={item['Id']} item={item} />
+                    <Ingredient key={item['Id']} item={item} section={section} />
                 )
             })}
         </ul>
