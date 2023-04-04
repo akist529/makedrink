7a4cc8d608ebc0be3099be73b3f78e9acf37727f
Alex Kist <akist0707@gmail.com>
2023-04-04 15:01:46 -0500

    Alter styling & DOM orientation of ingredients

--- src/components/ui/Ingredient/Ingredient.tsx
+++ src/components/ui/Ingredient/Ingredient.tsx
@@ -19,15 +19,13 @@ export default function Ingredient (props: { item: Item, section: Item[], setIng
     return (
         <li className={styles.Ingredient}>
             <div className={styles.info}>
-                { hasChildren && <button>
-                    <Image alt="Show Varieties" src={require(`/public/images/ui/more_vert.svg`)} width="8" height="64" />
-                </button> }
-                <div className={styles.name}>
-                    <span>{item['Name']}</span>
+                { hasChildren && <Image className={styles.children} alt="Show Varieties" src={require(`/public/images/ui/more_vert.svg`)} width="8" height="64" /> }
+                <div className={styles.icon}>
                     <Image alt={item['Name']} src={require(`/public/images/ui/${item['Name'].toLowerCase().split(" ").join("-").replaceAll("/", "-")}.webp`)} />
                 </div>
+                <IngredientCheckbox item={item} />
             </div>
-            <IngredientCheckbox item={item} />
+            <span>{item['Name']}</span>
         </li>
     )
 }
