f9a2025bafab15858ef0b7f52454b249c42a3ae4
Alex Kist <akist0707@gmail.com>
2023-04-06 08:56:41 -0500

    Add close button to ingredient modal

--- src/components/ui/IngredientModal/IngredientModal.tsx
+++ src/components/ui/IngredientModal/IngredientModal.tsx
@@ -1,14 +1,21 @@
 import styles from './IngredientModal.module.scss'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/store/store'
+import Image from 'next/image'
+import { useDispatch } from 'react-redux'
+import { toggleIngredientModal } from '@/store/slices/ingredientModal.slice'
 
 export default function IngredientModal() {
     const { ingredientModalOpen, modalIngredientID } = useSelector((state: RootState) => state.ingredientModal)
+    const dispatch = useDispatch()
 
     return (
         <>
             { ingredientModalOpen && <div className={styles.background}>
                 <div className={styles.modal}>
+                    <button className={styles.closeBtn} onClick={() => dispatch(toggleIngredientModal())}>
+                        <Image alt="Close Modal" src={require('/public/images/ui/close.svg')} />
+                    </button>
                     <span>{modalIngredientID}</span>
                 </div>
             </div> }
