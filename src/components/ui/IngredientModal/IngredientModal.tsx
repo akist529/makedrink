2b8bb1c647fc8cf8a4930fd887cfd1903476e3e9
Alex Kist <akist0707@gmail.com>
2023-04-06 14:13:53 -0500

    Add list of child ingredients to ingredient modal

--- src/components/ui/IngredientModal/IngredientModal.tsx
+++ src/components/ui/IngredientModal/IngredientModal.tsx
@@ -4,24 +4,32 @@ import { RootState } from '@/store/store'
 import Image from 'next/image'
 import { useDispatch } from 'react-redux'
 import { toggleIngredientModal } from '@/store/slices/ingredientModal.slice'
+import { useGetAllIngredientsQuery } from '@/store/api/api'
+import { Item } from '@/types/index'
 
 export default function IngredientModal() {
     const { ingredientModalOpen, modalIngredient } = useSelector((state: RootState) => state.ingredientModal)
     const dispatch = useDispatch()
+    const { data, isLoading, error } = useGetAllIngredientsQuery()
 
     return (
         <>
-            { ingredientModalOpen && <div className={styles.background}>
+            { ingredientModalOpen && data && <div className={styles.background}>
                 <div className={styles.modal}>
+                    <button onClick={() => dispatch(toggleIngredientModal())}>
+                        <Image alt="Close Modal" src={require('/public/images/ui/close.svg')} />
+                    </button>
                     <div className={styles.header}>
-                        <button onClick={() => dispatch(toggleIngredientModal())}>
-                            <Image alt="Close Modal" src={require('/public/images/ui/close.svg')} />
-                        </button>
                         <span>{modalIngredient['Name']}</span>
                         <Image alt={modalIngredient['Name']} src={require(`/public/images/ui/${modalIngredient['Name'].toLowerCase().split(' ').join('-').replace('/', '-')}.webp`)} />
                     </div>
+                    <div>
+                        { data.filter((ingredient: Item) => ingredient['AliasId'] === modalIngredient['Id']).map(ingredient => <span key={ingredient['Id']}>{ingredient['Name']}</span>) }
+                    </div>
                 </div>
             </div> }
+            { isLoading && <h1>Loading...</h1> }
+            { error && <h1>Error!</h1> }
         </>
     )
 }
