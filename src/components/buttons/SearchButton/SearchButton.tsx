1018eb2a57dffca1ecfac1e705725a2a25fe80b4
Alex Kist <akist0707@gmail.com>
2023-04-07 12:26:53 -0500

    Clean up JSX on SearchButton component

--- src/components/buttons/SearchButton/SearchButton.tsx
+++ src/components/buttons/SearchButton/SearchButton.tsx
@@ -8,10 +8,11 @@ import { toggleSearch } from '@/store/slices/search.slice'
 
 export default function SearchButton () {
     const dispatch = useDispatch()
+    const imagePath = require(`/public/images/ui/search.svg`)
 
     return (
         <button className={styles.search} onClick={() => dispatch(toggleSearch())}>
-            <Image alt='Search' src={require(`/public/images/ui/search.svg`)} width="40" height="40" />
+            <Image alt='Search' src={imagePath} width="40" height="40" />
         </button>
     )
 }
