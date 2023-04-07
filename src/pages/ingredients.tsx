4652f3958d3cb5e01fa2ab196a338467c8c69771
Alex Kist <akist0707@gmail.com>
2023-04-07 13:38:10 -0500

    Clean up JSX on Ingredients page

--- src/pages/ingredients.tsx
+++ src/pages/ingredients.tsx
@@ -13,67 +13,75 @@ import IngredientSection from '@/components/ui/IngredientSection/IngredientSecti
 export default function IngredientsPage() {
     const { data, isLoading, error } = useGetAllIngredientsQuery()
 
+    const ingredientsImagePath = require('/public/images/ui/local_bar.svg')
+    const alcoholImagePath = require('/public/images/ui/drunk.webp')
+    const mixerImagePath = require('/public/images/ui/shaker.webp')
+
+    function filterData (type: string[]) {
+        let filteredData: Item[] = []
+
+        for (let i = 0; i < type.length; i++) {
+            const categoryData = (data as Item[]).filter(ingredient => {
+                return ingredient['Type'] === type[i]
+            })
+
+            for (const item of categoryData) {
+                filteredData.push(item)
+            }
+        }
+
+        return filteredData
+    }
+
     return (
         <>
             { data && <div className={styles.IngredientsPage}>
-                <div>
-                    <h1>
-                        <div>
-                            { 'Select'.split('').map((letter, index) => {
-                                return (
-                                    <span key={index}>{letter}</span>
-                                )
-                            }) }
-                        </div>
-                        <div>
-                            { 'Ingredients'.split('').map((letter, index) => {
-                                return (
-                                    <span key={index}>{letter}</span>
-                                )
-                            }) }
-                        </div>
-                        <Image alt='Select Ingredients' src={require('/public/images/ui/local_bar.svg')} width='64' height='64' />
-                    </h1>
-                </div>
+                <h1>
+                    <div>
+                        {'Select'.split('').map((letter, index) => {
+                            return (
+                                <span key={index}>{letter}</span>
+                            )
+                        })}
+                    </div>
+                    <div>
+                        {'Ingredients'.split('').map((letter, index) => {
+                            return (
+                                <span key={index}>{letter}</span>
+                            )
+                        })}
+                    </div>
+                    <Image alt='Select Ingredients' src={ingredientsImagePath} width='64' height='64' />
+                </h1>
                 <div>
                     <div className={styles.category}>
                         <h2>Alcohol</h2>
-                        <Image alt="Alcohol" src={require('/public/images/ui/drunk.webp')} width="64" />
+                        <Image alt="Alcohol" src={alcoholImagePath} width="64" />
                     </div>
                     <IngredientCatBtn category="Spirits" color="pink" />
-                    <IngredientSection
-                        section={(data as Item[]).filter((ingredient: Item) => ingredient['Type'] === 'liquor')}
-                    />
+                    <IngredientSection section={filterData(['liquor'])} />
                     <IngredientCatBtn category="Liqueurs" color="green" />
-                    <IngredientSection
-                        section={(data as Item[]).filter((ingredient: Item) => ingredient['Type'] === 'liqueur')}
-                    />
+                    <IngredientSection section={filterData(['liqueur'])} />
                     <IngredientCatBtn category="Other" color="red" />
-                    <IngredientSection
-                        section={(data as Item[]).filter((ingredient: Item) => ingredient['Type'] === ('other') || ingredient['Type'] === ('wine'))}
-                    />
+                    <IngredientSection section={filterData(['other', 'wine'])} />
                 </div>
                 <div>
                     <div className={styles.category}>
                         <h2>Mixers</h2>
-                        <Image alt="Mixers" src={require('/public/images/ui/shaker.webp')} height="64" />
+                        <Image alt="Mixers" src={mixerImagePath} height="64" />
                     </div>
                     <IngredientCatBtn category="Carbonated" color="yellow" />
-                    <IngredientSection
-                        section={(data as Item[]).filter((ingredient: Item) => ingredient['Type'] === 'carbonated')}
-                    />
+                    <IngredientSection section={filterData(['carbonated'])} />
                     <IngredientCatBtn category="Juices" color="orange" />
-                    <IngredientSection
-                        section={(data as Item[]).filter((ingredient: Item) => ingredient['Type'] === 'juice')}
-                    />
+                    <IngredientSection section={filterData(['juice'])} />
                     <IngredientCatBtn category="Other" color="blue" />
-                    <IngredientSection
-                        section={(data as Item[]).filter((ingredient: Item) => ingredient['Type'] === 'mixer')}
-                    />
+                    <IngredientSection section={filterData(['mixer'])} />
                 </div>
             </div> }
-            { isLoading && <h1>Loading...</h1> }
-            { error && <h1>Error!</h1> }
+            { isLoading &&
+                <h1>Loading...</h1> }
+            { error &&
+                <h1>Error!</h1> }
         </>
     )
 }
