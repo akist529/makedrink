934c88fd2f7e7d2823b807a3131712c96626a473
Alex Kist <akist0707@gmail.com>
2023-04-07 12:42:51 -0500

    Clean up JSX on NavMenu component

--- src/components/navmenu/NavMenu.tsx
+++ src/components/navmenu/NavMenu.tsx
@@ -11,12 +11,15 @@ import NavMenuCategory from '@/components/navmenu/category/NavMenuCategory'
 import NavMenuItem from '@/components/navmenu/item/NavMenuItem'
 
 export default function NavMenu () {
-    const spirits = ['Whiskey', 'Brandy', 'Gin', 'Rum', 'Scotch', 'Tequila', 'Vermouth', 'Vodka', 'Absinthe']
+    // Redux selectors
     const { navMenuOpen } = useSelector((state: RootState) => state.navMenu)
+
+    const spirits = ['Whiskey', 'Brandy', 'Gin', 'Rum', 'Scotch', 'Tequila', 'Vermouth', 'Vodka', 'Absinthe']
     const dispatch = useDispatch()
+    const navStyles = [styles.navmenu, (navMenuOpen ?  styles.open : styles.closed)].join(' ')
 
     return (
-        <nav className={[styles.navmenu, (navMenuOpen ?  styles.open : styles.closed)].join(' ')}>
+        <nav className={navStyles}>
             <ul>
                 <Link href='/ingredients' onClick={() => dispatch(toggleNavMenu())}>
                     <NavMenuItem item='Select Ingredients' />
