cda2294b6e6c8301a3bc26a6ba54f1e300bdd8d1
Alex Kist <akist0707@gmail.com>
2023-05-09 14:35:10 -0500

    Fix code for Ingredient Filter component (moving from list-based stored ingredients to hash table solution)

--- src/components/ui/IngredientFilter/IngredientFilter.tsx
+++ src/components/ui/IngredientFilter/IngredientFilter.tsx
@@ -1,7 +1,9 @@
 // Import styles
 import styles from './IngredientFilter.module.scss'
+// React components
+import { useEffect } from 'react'
 // Redux components
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '@/store/store'
 // Type interfaces
 import { Item } from '@/types/index'
@@ -9,16 +11,23 @@ import { Item } from '@/types/index'
 export default function IngredientFilter (props: { type: string }) {
     const { type } = props
     const storedIngredients = useSelector((state: RootState) => state.ingredients.stored)
+    const dispatch = useDispatch();
 
-    const filteredIngredients: Item[] = (() => {
-        return storedIngredients.filter(ingredient => {
-            return ingredient['Type'] === type
-        })
+    const filteredIngredients = (() => {
+        const filteredIngredients = []
+
+        for (const key of Object.keys(storedIngredients[type])) {
+            for (const ingredient of storedIngredients[type][key]) {
+                filteredIngredients.push(ingredient);
+            }
+        }
+
+        return filteredIngredients;
     })()
 
     return (
         <div className={styles.IngredientFilter}>
-            { filteredIngredients.map((ingredient: Item, index) => {
+            { filteredIngredients.map((ingredient: Item, index: number) => {
                 return <div key={index}>
                     <label htmlFor={ingredient['Name']}>{ingredient['Name']}</label>
                     <input type="checkbox" id={ingredient['Name']} name={ingredient['Name']} value={ingredient['Name']}/>
