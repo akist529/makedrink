7ec661a2e8657d452352500ebe9cd38b575a0707
Alex Kist <akist0707@gmail.com>
2023-05-10 14:05:26 -0500

    Add getIngredientFromStore function to RandomDrink component to find alternative ingredients if needed

--- src/components/ui/RandomDrink/RandomDrink.tsx
+++ src/components/ui/RandomDrink/RandomDrink.tsx
@@ -10,6 +10,30 @@ import Image from 'next/image'
 export default function RandomDrink (props: { randomDrink: any }) {
     const { randomDrink } = props;
     const possibleDrinks = useSelector((state: RootState) => state.drinks.possible);
+    const storedIngredients = useSelector((state: RootState) => state.ingredients.stored);
+
+    function getIngredientFromStore (ingredient: any, index: number) {
+        const letter = ingredient.Name.charAt(0);
+
+        for (const key of Object.keys(storedIngredients)) {
+            if (storedIngredients[key].hasOwnProperty(letter)
+                && storedIngredients[key][letter].find((item: any) => item.Name === ingredient.Name)) {
+                return (
+                    <span key={index}>{ingredient.Name}</span>
+                );
+            }
+        }
+
+        for (const key of Object.keys(storedIngredients)) {
+            for (const letter of Object.keys(storedIngredients[key])) {
+                if (storedIngredients[key][letter].find((item: any) => item.Name === ingredient.Alias)) {
+                    return (
+                        <span key={index}>{ingredient.Alias}</span>
+                    );
+                }
+            }
+        }
+    }
 
     return (
         <div>
@@ -18,7 +42,7 @@ export default function RandomDrink (props: { randomDrink: any }) {
                 <span>{randomDrink['Name']}</span>
                 <span>DRINK IMAGE</span>
                 { randomDrink['Recipe'].map((ingredient: any, index: number) => {
-                    return <span key={index}>{ingredient['Name']}</span>
+                    return getIngredientFromStore(ingredient, index)
                 }) }
                 <Link href={`/drink/${randomDrink.Name.toLowerCase().replaceAll(' ', '-')}`}>
                     <button>
