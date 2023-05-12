d6fffca5920776dcdf8d52c12192ab66b858a220
Alex Kist <akist0707@gmail.com>
2023-05-12 15:02:39 -0500

    Work on integrating TypeScript interface for stored ingredients

--- src/components/ui/Ingredient/Ingredient.tsx
+++ src/components/ui/Ingredient/Ingredient.tsx
@@ -9,23 +9,23 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useGetAllIngredientsQuery } from '@/store/api/api'
 import { toggleIngredientModal, setModalIngredient } from '@/store/slices/ingredientModal.slice'
 import { addIngredient, removeIngredient } from '@/store/slices/ingredients.slice'
-import { RootState, store } from '@/store/store'
+import { RootState } from '@/store/store'
 // Local components
 import IngredientCheckbox from '@/components/inputs/IngredientCheckbox/IngredientCheckbox'
 // Type interfaces
-import { Item, DrinkInfo } from '@/types/index'
+import { Item, Dictionary, Type } from '@/types/index'
 
 export default function Ingredient (props: { item: Item, section: Item[]}) {
     // Import props
     const {item, section} = props;
-    // React states
-    const [isChecked, setIsChecked] = useState(false)
     // Redux components
-    const storedIngredients = useSelector((state: RootState) => state.ingredients.stored)
+    const storedIngredients: { [key: string]: Type } = useSelector((state: RootState) => state.ingredients.stored)
     const dispatch = useDispatch()
-    const ingredientImagePath = require(`/public/images/ui/${item['Name'].toLowerCase().split(" ").join("-").replaceAll("/", "-")}.webp`)
-    const childrenImagePath = require(`/public/images/ui/more_vert.svg`)
-    const allIngredients = (useGetAllIngredientsQuery().data || []);
+    const ingredientImagePath: typeof Image = require(`/public/images/ui/${item['Name'].toLowerCase().split(" ").join("-").replaceAll("/", "-")}.webp`)
+    const childrenImagePath: typeof Image = require(`/public/images/ui/more_vert.svg`)
+    const allIngredients: Item[] = (useGetAllIngredientsQuery().data || []);
+    // React states
+    const [isChecked, setIsChecked] = useState(itemInStore(item));
 
     const hasChildren = (() => {
         if (!item.AliasId) {
@@ -34,70 +34,47 @@ export default function Ingredient (props: { item: Item, section: Item[]}) {
                     return true;
                 }
             }
+        } return false;
+    })();
+
+    function itemInStore (item: Item) {
+        const letter = item.Name.charAt(0);
+        const type = item.Type;
+
+        if (storedIngredients.hasOwnProperty(type)
+            && storedIngredients[type].hasOwnProperty(letter)) {
+            const path = storedIngredients[type][letter];
+
+            if (path.some((ingredient: Item) => ingredient.Id === item.Id)) {
+                return true;
+            }
         }
 
         return false;
-    })();
+    }
 
-    // If parent ingredient, see if child ingredient is in store
-    useEffect(() => {
-        if (hasChildren && !isChecked && storedIngredients.hasOwnProperty(item.Type)) {
+    function aliasInStore(item: Item) {
+        if (storedIngredients.hasOwnProperty(item.Type)) {
             for (const key of Object.keys(storedIngredients[item.Type])) {
-                for (const ingredient of storedIngredients[item.Type][`${key}`]) {
-                    if (ingredient.AliasId === item.Id) {
-                        setIsChecked(true);
-                        break;
-                    }
-                }
-
-                if (isChecked) {
-                    break;
+                const path = storedIngredients[item.Type][key];
+                
+                if (path.some((ingredient: Item) => ingredient.AliasId === item.Id)) {
+                    return true;
                 }
             }
         }
-    }, [hasChildren]) // Execute if ingredient is denoted as having children
 
-
-    // Fill checkbox if ingredient is in store
-    // useEffect(() => {
-    //     // See if child ingredient is in store
-    //     function includesAlias () {
-    //         for (const key of Object.keys(storedIngredients[`${item.Type}`])) {
-    //             for (const ingredient of storedIngredients[`${item.Type}`][`${key}`]) {
-    //                 if (ingredient.AliasId === item.Id) {
-    //                     return true;
-    //                 }
-    //             }
-    //         }
-
-    //         return false;
-    //     }
-
-    //     const letter = item.Name.charAt(0);
-
-    //     if (hasChildren && storedIngredients.hasOwnProperty(item.Type) && includesAlias()) {
-    //         setIsChecked(true);
-    //     } else if (storedIngredients.hasOwnProperty(item.Type) 
-    //         && storedIngredients[`${item.Type}`].hasOwnProperty(letter)) {
-    //         if (storedIngredients[`${item.Type}`][`${letter}`].find((ingredient: Item) => ingredient.Name === item.Name)) {
-    //             setIsChecked(true);
-    //         } else {
-    //             setIsChecked(false);
-    //         }
-    //     } else {
-    //         setIsChecked(false);
-    //     }
-    // }, [storedIngredients]) // Execute if stored ingredients updates
-
+        return false;
+    }
 
     function addIngredientToStore () {
         dispatch(addIngredient(item))
 
         // Add parent ingredient to store if applicable
         if (item.AliasId) {
-            for (let i = 0; i < allIngredients.length; i++) {
-                if (allIngredients[i].Id === item.AliasId) {
-                    dispatch(addIngredient(allIngredients[i]))
+            for (const ingredient of allIngredients) {
+                if (ingredient.Id === item.AliasId) {
+                    dispatch(addIngredient(ingredient));
                     break;
                 }
             }
@@ -130,6 +107,16 @@ export default function Ingredient (props: { item: Item, section: Item[]}) {
         }
     }
 
+    // If parent ingredient, see if child ingredient is in store
+    useEffect(() => {
+        if (hasChildren) {
+            if (aliasInStore(item)) {
+                setIsChecked(true);
+            } else {
+                setIsChecked(false);
+            }
+        }
+    }, [storedIngredients]);
 
     return (
         <li className={styles.Ingredient}>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,25 +1,64 @@
 export interface Item {
-    Id: number
-    Name: string
-    AliasId: number
-    Type: string
+    Id: number;
+    Name: string;
+    AliasId: number;
+    Type: string;
 }
 
 export interface Ingredient {
-    Name: string
-    IsAlias: boolean
-    Alias: string
-    Amount: number
-    Unit: string
+    Name: string;
+    IsAlias: boolean;
+    Alias: string;
+    Amount: number;
+    Unit: string;
 }
 
 export interface DrinkInfo {
-    Name: string
-    Recipe: Ingredient[]
-    Directions: string[]
+    Name: string;
+    Recipe: Ingredient[];
+    Directions: string[];
 }
 
 export interface Drink {
-    Id: number
-    Name: string
+    Id: number;
+    Name: string;
+}
+
+export interface Type {
+    A?: Item[];
+    B?: Item[];
+    C?: Item[];
+    D?: Item[];
+    E?: Item[];
+    F?: Item[];
+    G?: Item[];
+    H?: Item[];
+    I?: Item[];
+    J?: Item[];
+    K?: Item[];
+    L?: Item[];
+    M?: Item[];
+    N?: Item[];
+    O?: Item[];
+    P?: Item[];
+    Q?: Item[];
+    R?: Item[];
+    S?: Item[];
+    T?: Item[];
+    U?: Item[];
+    V?: Item[];
+    W?: Item[];
+    X?: Item[];
+    Y?: Item[];
+    Z?: Item[];
+}
+
+export interface Dictionary {
+    liquor?: Type;
+    liqueur?: Type;
+    other?: Type;
+    wine?: Type;
+    carbonated?: Type;
+    juice?: Type;
+    mixer?: Type;
 }
