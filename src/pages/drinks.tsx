bbbbc95a88519ac8b516c13e9c97f96ae65f14ba
Alex Kist <akist0707@gmail.com>
2023-05-21 00:27:09 -0500

    Limit quantity of drinks page items to 20 and add pagination links to view items

--- src/pages/drinks.tsx
+++ src/pages/drinks.tsx
@@ -2,6 +2,8 @@
 import styles from '@/styles/Drinks.module.scss';
 // Next components
 import type { NextPage } from 'next';
+// React components
+import { useState } from 'react';
 // Redux components
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
@@ -12,15 +14,59 @@ import { DrinkDict, DrinkInfo } from '@/types/index';
 
 const DrinksPage: NextPage = () => {
     const possibleDrinks: DrinkDict = useSelector((state: RootState) => state.drinks.possible);
+    const [firstDrink, setFirstDrink] = useState(0);
+    const [lastDrink, setLastDrink] = useState(20);
+
+    const drinksList = (() => {
+        const arr = [];
+
+        for (const key of Object.keys(possibleDrinks)) {
+            for (const item of possibleDrinks[key]) {
+                arr.push(item);
+            }
+        }
+
+        return arr;
+    })();
+
+    function changePage (pages: string) {
+        const firstNum = Number(pages.replaceAll(' ', '').split('-')[0]);
+        const secondNum = Number(pages.replaceAll(' ', '').split('-')[1]);
+
+        setFirstDrink(firstNum);
+        setLastDrink(secondNum);
+    }
 
     return (
         <main className={styles.DrinksPage}>
+            <nav>
+                <ul>
+                    { (() => {
+                        const pageNums = [];
+
+                        for (let i = 1; i < drinksList.length; i++) {
+                            const firstNum = i;
+                            const secondNum = ((i + 20) > drinksList.length) ? drinksList.length : (i + 20);
+                            pageNums.push(`${firstNum} - ${secondNum}`);
+                            i += 20;
+                        }
+
+                        return pageNums;
+                    })().map((pages: string, index: number) => {
+                        return (
+                            <li key={index}>
+                                <button className={styles.pageBtn} onClick={() => changePage(pages)}>
+                                    <span>{pages}</span>
+                                </button>
+                            </li>
+                        );
+                    }) }
+                </ul>
+            </nav>
             <section>
                 <ul>
-                    { Object.keys(possibleDrinks).map((key: string, index: number) => {
-                        return possibleDrinks[key].map((drink: DrinkInfo, index: number) => {
-                            return <DrinkCard drink={drink} key={index} />
-                        })
+                    { drinksList.slice(firstDrink, lastDrink).map((drink: DrinkInfo, index: number) => {
+                        return <DrinkCard drink={drink} key={index} />
                     }) }
                 </ul>
             </section>
