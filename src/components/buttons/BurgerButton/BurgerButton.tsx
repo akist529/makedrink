35a50457e58e8dfe06c3fd9b3a25ac6687493433
akist529 <akist0707@gmail.com>
2023-05-22 10:37:47 -0500

    Refactor BurgerButton component

--- src/components/buttons/BurgerButton/BurgerButton.tsx
+++ src/components/buttons/BurgerButton/BurgerButton.tsx
@@ -1,23 +1,30 @@
 // Component styles
-import styles from './BurgerButton.module.scss'
+import styles from './BurgerButton.module.scss';
+// React components
+import { useState, useEffect } from 'react';
 // Next components
-import Image from 'next/image'
+import Image from 'next/image';
 // Redux components
-import { useSelector, useDispatch } from 'react-redux'
-import { RootState } from '@/store/store'
-import { toggleNavMenu } from '@/store/slices/navMenu.slice'
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '@/store/store';
+import { toggleNavMenu } from '@/store/slices/navMenu.slice';
 
 export default function BurgerButton() {
-    // Redux selectors
-    const { navMenuOpen } = useSelector((state: RootState) => state.navMenu)
-    
-    const dispatch = useDispatch()
-    const imagePathOpen = require('public/images/ui/menu_open.svg')
-    const imagePathClosed = require('public/images/ui/menu.svg')
+    const { navMenuOpen } = useSelector((state: RootState) => state.navMenu);
+    const [imagePath, setImagePath] = useState('public/images/ui/menu_open.svg');
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (navMenuOpen) {
+            setImagePath(require('public/images/ui/menu_open.svg'));
+        } else {
+            setImagePath(require('public/images/ui/menu.svg'));
+        }
+    }, [navMenuOpen]);
 
     return (
-        <button className={styles.burger} onClick={() => dispatch(toggleNavMenu())}>
-            <Image alt='Open Menu' src={navMenuOpen ? imagePathOpen : imagePathClosed} width="40" height="40" />
+        <button className={styles.BurgerButton} onClick={() => dispatch(toggleNavMenu())}>
+            <Image alt='Open Nav Menu' src={imagePath} width="40" height="40" />
         </button>
     )
 }
