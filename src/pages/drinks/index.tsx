d923d346e717bec03e100fcf96fd4be2e464486f
akist529 <akist0707@gmail.com>
2023-05-24 13:39:20 -0500

    Update logic for all drinks page to remove blocked drinks

--- src/pages/drinks/index.tsx
+++ src/pages/drinks/index.tsx
@@ -5,6 +5,8 @@ import type { NextPage } from 'next';
 // React components
 import { useState, useEffect } from 'react';
 // Redux components
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store/store';
 import { useGetAllDrinkInfoQuery } from '@/store/api/api';
 // Local components
 import DrinkCard from '@/components/ui/DrinksPage/DrinkCard/DrinkCard';
@@ -17,9 +19,24 @@ const AllDrinksPage: NextPage = () => {
     const [firstDrink, setFirstDrink] = useState(0);
     const [lastDrink, setLastDrink] = useState(20);
     const [pageNums, setPageNums] = useState([] as string[]);
+    const [drinksList, setDrinksList] = useState([] as DrinkInfo[]);
+    const blockedDrinks = useSelector((state: RootState) => state.drinks.blocked);
 
     useEffect(() => {
         if (!isLoading) {
+            const allDrinks = data || [];
+            const arr = [];
+
+            for (const item of allDrinks) {
+                if (blockedDrinks.hasOwnProperty(item.Name.charAt(0))) {
+                    if (blockedDrinks[item.Name.charAt(0)].find((drink: DrinkInfo) => drink.Name === item.Name)) {
+                        continue;
+                    }
+                }
+
+                arr.push(item);
+            }
+
             const pageNumsArr = (() => {
                 const arr = [];
         
@@ -34,6 +51,7 @@ const AllDrinksPage: NextPage = () => {
             })();
 
             setPageNums(pageNumsArr);
+            setDrinksList(arr);
         }
     }, [isLoading]);
 
@@ -55,7 +73,7 @@ const AllDrinksPage: NextPage = () => {
                     setLastDrink={setLastDrink} />
                 <section>
                     <ul>
-                        { (data || []).slice(firstDrink, lastDrink).map((drink: DrinkInfo, index: number) => {
+                        { drinksList.slice(firstDrink, lastDrink).map((drink: DrinkInfo, index: number) => {
                             return (<DrinkCard drink={drink} key={index} />);
                         }) }
                     </ul>
