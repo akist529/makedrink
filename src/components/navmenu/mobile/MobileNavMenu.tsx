c965002e2bae6daec9999cf1fb6e00211bd768bb
Alex Kist <akist0707@gmail.com>
2023-06-04 13:59:52 -0500

    Fix issue with nav menu disappearing when navigating links in tablet and laptop/desktop view sizes

--- src/components/navmenu/mobile/MobileNavMenu.tsx
+++ src/components/navmenu/mobile/MobileNavMenu.tsx
@@ -1,5 +1,7 @@
 // Component styles
 import styles from './MobileNavMenu.module.scss';
+// React components
+import { useEffect } from 'react';
 // Next components
 import Link from 'next/link';
 // Redux components
@@ -16,40 +18,46 @@ export default function MobileNavMenu () {
 
     const navStyles = [styles.NavMenu, (navMenuOpen ?  styles.open : styles.closed)].join(' ');
 
+    function handleClick () {
+        if (window.innerWidth < 600) {
+            dispatch(toggleNavMenu());
+        }
+    }
+
     return (
         <nav className={navStyles}>
             <ul>
-                <Link href='/' onClick={() => dispatch(toggleNavMenu())}>
+                <Link href='/' onClick={handleClick}>
                     <NavMenuItem 
                         item='Make A Drink' 
                         img='liquor.svg' />
                 </Link>
-                <Link href='/ingredients' onClick={() => dispatch(toggleNavMenu())}>
+                <Link href='/ingredients' onClick={handleClick}>
                     <NavMenuItem 
                         item='Select Ingredients' 
                         img='blender.svg' />
                 </Link>
-                <Link href='/drinks' onClick={() => dispatch(toggleNavMenu())}>
+                <Link href='/drinks' onClick={handleClick}>
                     <NavMenuItem 
                         item='All Drinks' 
                         img='local_bar.svg' />
                 </Link>
-                <Link href='/drinks/possible' onClick={() => dispatch(toggleNavMenu())}>
+                <Link href='/drinks/possible' onClick={handleClick}>
                     <NavMenuItem 
                         item='Possible Drinks' 
                         img='done_all.svg' />
                 </Link>
-                <Link href='/drinks/filtered' onClick={() => dispatch(toggleNavMenu())}>
+                <Link href='/drinks/filtered' onClick={handleClick}>
                     <NavMenuItem 
                         item='Filtered Drinks' 
                         img='filter_list.svg' />
                 </Link>
-                <Link href='/drinks/favorited' onClick={() => dispatch(toggleNavMenu())}>
+                <Link href='/drinks/favorited' onClick={handleClick}>
                     <NavMenuItem 
                         item='Favorite Drinks' 
                         img='favorite.svg' />
                 </Link>
-                <Link href='/drinks/blocked' onClick={() => dispatch(toggleNavMenu())}>
+                <Link href='/drinks/blocked' onClick={handleClick}>
                     <NavMenuItem 
                         item='Blocked Drinks' 
                         img='block.svg' />
