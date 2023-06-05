adbf62625d6c6ff8bc0e53661d053dfe14e38b9d
akist529 <akist0707@gmail.com>
2023-06-05 09:53:54 -0500

    Add unique color to active nav menu link

--- src/components/navmenu/item/NavMenuItem.tsx
+++ src/components/navmenu/item/NavMenuItem.tsx
@@ -2,13 +2,15 @@
 import styles from './NavMenuItem.module.scss';
 // Next components
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-export default function NavMenuItem(props: { item: string, img: string }) {
-    const { item, img } = props;
+export default function NavMenuItem(props: { item: string, slug: string, img: string }) {
+    const { item, slug, img } = props;
     const imagePath = require(`/public/images/ui/${img}`);
+    const router = useRouter();
 
     return (
-        <button className={styles.NavMenuItem}>
+        <button className={(router.pathname === slug) ? [styles.NavMenuItem, styles.active].join(' ') : styles.NavMenuItem}>
             <span>{item}</span>
             <Image 
                 alt={item} 
--- src/components/navmenu/mobile/MobileNavMenu.tsx
+++ src/components/navmenu/mobile/MobileNavMenu.tsx
@@ -30,36 +30,43 @@ export default function MobileNavMenu () {
                 <Link href='/' onClick={handleClick}>
                     <NavMenuItem 
                         item='Make A Drink' 
+                        slug='/' 
                         img='liquor.svg' />
                 </Link>
                 <Link href='/ingredients' onClick={handleClick}>
                     <NavMenuItem 
                         item='Select Ingredients' 
+                        slug='/ingredients' 
                         img='blender.svg' />
                 </Link>
                 <Link href='/drinks' onClick={handleClick}>
                     <NavMenuItem 
                         item='All Drinks' 
+                        slug='/drinks' 
                         img='local_bar.svg' />
                 </Link>
                 <Link href='/drinks/possible' onClick={handleClick}>
                     <NavMenuItem 
                         item='Possible Drinks' 
+                        slug='/drinks/possible' 
                         img='done_all.svg' />
                 </Link>
                 <Link href='/drinks/filtered' onClick={handleClick}>
                     <NavMenuItem 
                         item='Filtered Drinks' 
+                        slug='/drinks/filtered' 
                         img='filter_list.svg' />
                 </Link>
                 <Link href='/drinks/favorited' onClick={handleClick}>
                     <NavMenuItem 
                         item='Favorite Drinks' 
+                        slug='/drinks/favorited' 
                         img='favorite.svg' />
                 </Link>
                 <Link href='/drinks/blocked' onClick={handleClick}>
                     <NavMenuItem 
                         item='Blocked Drinks' 
+                        slug='/drinks/blocked' 
                         img='block.svg' />
                 </Link>
             </ul>
