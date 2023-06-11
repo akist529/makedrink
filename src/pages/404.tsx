504bdcf0f33032b755c6b5ecb6208341ef4428c8
Alex Kist <akist0707@gmail.com>
2023-06-11 18:45:42 -0500

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,26 @@
+// Page styles
+import styles from '@/styles/Error.module.scss';
+// Next components
+import type { NextPage } from 'next';
+import Link from 'next/link';
+// Local components
+import MakeDrinkButton from '@/components/buttons/MakeDrinkButton/MakeDrinkButton';
+import Footer from '@/components/footer/Footer';
+
+const ErrorPage: NextPage = () => {
+    return (
+        <div className={styles.ErrorPage}>
+            <h1>Maybe you&apos;ve had enough...</h1>
+            <hr/>
+            <h2>The URL that you tried to access does not exist.</h2>
+            <Link href='/'>
+                <nav>
+                    <MakeDrinkButton />
+                </nav>
+            </Link>
+            <Footer />
+        </div>
+    )
+}
+
+export default ErrorPage;
