4c6edda82e6c05a7c96ff9fb74527630963dccb9
Alex Kist <akist0707@gmail.com>
2023-06-14 17:12:22 -0500

    Update stying for login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,31 +2,13 @@
 import styles from '@/styles/Login.module.scss';
 // Next components
 import type { NextPage } from 'next';
+// Local components
+import LoginCard from '@/components/ui/LoginPage/LoginCard/LoginCard';
 
 const LoginPage: NextPage = () => {
     return (
         <div className={styles.LoginPage}>
-            <div className={styles.LoginCard}>
-                <header>
-                    <strong>Sign In</strong>
-                    <span>To Access Admin Features</span>
-                </header>
-                <form action="" method="get">
-                    <label htmlFor="email">E-Mail:</label>
-                    <input 
-                        type="email" 
-                        id="email" 
-                        name="email" 
-                        placeholder="E-Mail"/><br/>
-                    <label htmlFor="pass">Password:</label>
-                    <input 
-                        type="password" 
-                        id="pass" 
-                        name="pass"
-                        placeholder="Password"/><br/>
-                    <input type="submit" value="Submit"/>
-                </form>
-            </div>
+            <LoginCard />
         </div>
     );
 }
